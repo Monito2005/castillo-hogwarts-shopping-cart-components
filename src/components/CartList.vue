de51d5607312cf9481280e081a17cb8f2d97685e
Monito2005 <gustavocastillo885@gmail.com>
2026-03-11 18:56:10 -0500

    Use props

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <CartListItem></CartListItem>
-    <CartListItem></CartListItem>
+        <CartListItem v-for="item in cartItems" :key="item.id" :item="item"></CartListItem>
     </div>
 </template>
 
 <script setup>
 import CartListItem from '@/components/CartListItem.vue'
+defineProps(['cartItems'])
 </script>
 
 <style scoped></style>
